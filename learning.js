25136184b59b9fe59bf3fdb35333108dbd75a806
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2019-03-15 21:00:40 -0700

    sanity

---
+++ learning.js
@@ -0,0 +1,106 @@
+/*
+WHAT IS IT?
+CAN YOU GIVE ME AN EXAMPLE OF CODE TO WRITE?
+CAN YOU REMEMBER IF YOU HAVE USED IT WHILE WORKING?*/
+/*
+- PRIMITIVE DATA TYPES
+  * strings
+  * Numbers
+  * Booleans
+  * null
+  * undefined
+  * Symbols (ES6)
+  */
+
+
+
+
+
+/*
+WHAT IS IT?
+CAN YOU GIVE ME AN EXAMPLE OF CODE TO WRITE?
+CAN YOU REMEMBER IF YOU HAVE USED IT WHILE WORKING?*/
+/*
+- OBJECTS
+  * Arrays
+  * Object literals
+  * Functions
+*/
+
+
+
+
+/*
+WHAT IS IT?
+CAN YOU GIVE ME AN EXAMPLE OF CODE TO WRITE?
+CAN YOU REMEMBER IF YOU HAVE USED IT WHILE WORKING?*/
+/*
+- NOT A PRIMITIVE AND NOT AN OBJECT?
+NaN
+//
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://www.sololearn.com/Play/JavaScript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+WHAT IS IT?
+CAN YOU GIVE ME AN EXAMPLE OF CODE TO WRITE?
+CAN YOU REMEMBER IF YOU HAVE USED IT WHILE WORKING?*/
+/*
+- OPERATORS
+  * Math operators
+  * Assignment operators
+  * Comparison operators
+  * Boolean operators
+  * String operators
+*/
--- playground.js
+++ playground.js
@@ -1,1 +1,16 @@
+function transform(data) {
+  var replaceArr = [
+    {oldStr: /^Diamond+/i, newStr:""},
+    {oldStr: /(-Shop\s*now\.?|Learn\s*more\.?)/i, newStr: ""}
+  ];
 
+  replaceArr.forEach(function(el) {
+    data = data.replace(el.oldStr, el.newStr);
+  });
+
+  while (data.length > 100 && data.indexOf(".") !== data.lastIndexOf(".")) {
+    data = data.slice(0, data.lastIndexOf(".") + 1);
+  }
+
+  return data.trim();
+}
