98e294759cbe67a129a78148bbaedd4b39f00cf3
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2019-05-07 19:07:34 -0700

    working on evalArray

--- copypaste/cp-playground.js
+++ copypaste/cp-playground.js
@@ -1,64 +1,89 @@
 function evalArr(arrStr) {
-  arrStr = arrStr.slice(1, arrStr.length-1);
-  console.log("The arrStr after the slice:", arrStr);
-  let finalArr = [];
-  let openCharsReg = /[\[\{"'`]/;
-  let openCharsStr = "[{\"'``"
-  let openCloseCharsArr = [
-    {
-      openChar: "[",
-      closeChar: "]",
-      relFun: evalArr;
-    },
-    {
-      openChar: "{",
-      closeChar: "}",
-      relFun: evalObj;
-    },
-    {
-      openChar: '"',
-      closeChar: '"',
-      relFun: evalString;
-    },
-    {
-      openChar: "'",
-      closeChar: "'",
-      relFun: evalString;
-    },
-    {
-      openChar: "`",
-      closeChar: "`",
-      relFun: evalString;
-    }
-  ];
+  console.log("In the evalArray for", arrStr);
+  if (arrStr[0] !== "[" || arrStr[arrStr.length-1] !== "]"){
+    console.log("In the error because the first or the last character of the string do not indicate an array");
+    this.errorEntry = true;
+  }
+  else {
+    arrStr = arrStr.slice(1, arrStr.length-1);
+    console.log("The arrStr after the slice:", arrStr);
+    let arrElements = []; //to be returned at the end of the function
+    let bracketsObjects = [
+      {
+        openBrkt: "[",
+        closeBrkt: "]",
+        runFnctn: this.evalArr;
+      },
+      {
+        openBrkt: "{",
+        closeBrkt: "}",
+        runFnctn: this.evalObj;
+      }
+    ];
+    let openBracketsReg = /[\[\{]/;
+    let openBracketsStr = "[{";
+    let closeBracketsStr = "]}";
 
-  // while (arrStr.length > 0) {
-  //
-  // }
+    /*This is the work that I am doing on the playground file.
+    Objective is to properly split the string in the commas*/
+
+    //Will open the while loop when I have figured out how to properly do the splits
+    // while (arrStr.length > 0) {
+      //
+      // }
 
 
-  if (arrStr.search(openCharsReg) === 0) {
-    let position = openCharsStr[arrStr[0]];
+
+
+
+  }// End of the else statement
+
+
+
+
+  //If the first character in the string arrStr is [ {
+  //that means that the first element is an array or object
+  //Other if statements need to follow for undefined, true, false, null, callback
+  //function, immediately invoked function and string
+  if (arrStr.indexOf("[") === 0 || arrStr.indexOf("{") === 0) {
     console.log("In the if");
-    if (sliceArrObj(openCloseCharsArr[position].openChar, openCloseCharsArr[position].closeChar, arrStr)) {
-      console.log("This is what I will push:", arrStr.substring(0, sliceArrObj(openCloseCharsArr[position].openChar, openCloseCharsArr[position].closeChar, arrStr)));
-      finalArr.push(openCloseCharsArr[position].relFun(arrStr.substring(0, sliceArrObj(openCloseCharsArr[position].openChar, openCloseCharsArr[position].closeChar, arrStr))));
-      arrStr = arrStr.slice(sliceArrObj(openCloseCharsArr[position].openChar, openCloseCharsArr[position].closeChar, arrStr));
+    let idx = openBracketsStr.indexOf(arrStr[0]); //This is the index of
+    //the character that is represented in the array bracketsObjects
+
+    //If the string has a correct opening and closing brackets
+    //(from the sliceArrObj function) then slice to the correct idx AND
+    //Invoke the related function to evaluate the string, array or object
+    if (sliceArrObj(bracketsObjects[idx].openBrkt, bracketsObjects[idx].closeBrkt, arrStr)) {
+      console.log("This is what I will push:", arrStr.substring(0, sliceArrObj(bracketsObjects[idx].openBrkt, bracketsObjects[idx].closeBrkt, arrStr)));
+      //Push in arrElements array the function for evaluating arrays or objects (recursively), with the related substring
+      arrElements.push(bracketsObjects[idx].runFnctn(arrStr.substring(0, sliceArrObj(bracketsObjects[idx].openBrkt, bracketsObjects[idx].closeBrkt, arrStr))));
+      //slice the arrayString at the level of the substring
+      arrStr = arrStr.slice(sliceArrObj(bracketsObjects[idx].openBrkt, bracketsObjects[idx].closeBrkt, arrStr));
       arrStr = arrStr.trim();
     }
-    else return "error";
+    else this.errorEntry = true;
   }
 
-  return finalArr;
-}
+  /*
+  Need to write if statements for
+  strings
+  undefined
+  null
+  true
+  false
+  numbers 
+  */
+
+  return arrElements;
+} //End of evalArr function
 
 
-function sliceArrObj(openChar, closeChar, arr) {
+function sliceArrObj(openBrkt, closeBrkt, arr) {
     let counter = 1;
     let subStr = arr.slice(1);
     for (let i = 0; i < subStr.length; i++) {
-      if(subStr[i] === openChar) counter++;
-      else if(subStr[i] === closeChar){
+      if(subStr[i] === openBrkt) counter++;
+      else if(subStr[i] === closeBrkt){
         counter--;
         if(counter === 0) return i+2;
       }
