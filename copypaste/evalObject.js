e922dcfe7eacf6b4e3f0aa97b4f74ca7748719ee
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2019-06-12 22:19:13 -0700

    work

--- copypaste/evalObject.js
+++ copypaste/evalObject.js
@@ -1,4 +1,4 @@
-evalObjectvar test1 = "[1, 2, 3]"; //ok
+var test1 = "[1, 2, 3]"; //ok
 var test2 = "[1, 2, 'Hello']" //ok
 var test3 = "[1, 2, [3, 4]]" //ok
 var test4 = "[function (){return 'I love it'}, 88, undefined, null, 'grand array']"; //ok
@@ -21,11 +21,11 @@ var test1 =
 /*
 WHEN YOU ARE DONE, YOU NEED TO CHANGE THE sliceArrObj TO this.sliceArrObj
 the evalObject( to this.evalObject(
-and the evalObjectElements to this.evalObjectElements
+and the evalArrayElements to this.evalArrayElements
 */
 
-function evalObject(objStr, evalObjectElements) {
-  if (objStr === "{}") return evalObjectElements;
+function evalObject(objStr, evalArrayElements) {
+  if (objStr === "{}") return evalArrayElements;
   else {
     if (objStr[0] !== "{" || objStr[objStr.length-1] !== "}"){
       console.log("In the error because the first or the last character of the string do not indicate an array");
@@ -41,49 +41,49 @@ function evalObject(objStr, evalObjectElements) {
       //Number
       if(objStr.search(/\d+(\.\d+)?,/) === 0 || objStr.search(/\d+(\.\d+)?\s*$/) === 0) {
         console.log("else if for number");
-        evalObjectElements.push(Number(objStr.match(/\d+(\.\d+)?/)[0]));
+        evalArrayElements.push(Number(objStr.match(/\d+(\.\d+)?/)[0]));
         console.log(Number(objStr.match(/\d+(\.\d+)?/)[0]));
         objStr = objStr.slice(objStr.match(/\d+(\.\d+)?/)[0].length + 1);
         // objStr = objStr.slice(objStr.indexOf(",") + 1);
         objStr = objStr.trim();
         console.log("end of else if");
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //true
       else if (objStr.indexOf("true,") === 0 || objStr.search(/true\s*$/) === 0) {
         console.log("else if for true");
-        evalObjectElements.push(true);
+        evalArrayElements.push(true);
         objStr = objStr.slice(5);
         objStr = objStr.trim();
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //false
       else if (objStr.indexOf("false,") === 0 || objStr.search(/false\s*$/) === 0) {
         console.log("else if for false");
-        evalObjectElements.push(false);
+        evalArrayElements.push(false);
         objStr = objStr.slice(6);
         objStr = objStr.trim();
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //null
       else if (objStr.indexOf("null,") === 0 || objStr.search(/null\s*$/) === 0) {
         console.log("else if for null");
-        evalObjectElements.push(null);
+        evalArrayElements.push(null);
         objStr = objStr.slice(5);
         objStr = objStr.trim();
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //Undefined
       else if (objStr.indexOf("undefined,") === 0 || objStr.search(/undefined\s*$/) === 0) {
         console.log("else if for undefined");
-        evalObjectElements.push(undefined);
+        evalArrayElements.push(undefined);
         objStr = objStr.slice(10);
         objStr = objStr.trim();
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //string
@@ -91,15 +91,15 @@ function evalObject(objStr, evalObjectElements) {
         console.log("else if for string");
         let firstQuote = objStr[0];
         if (objStr.indexOf(firstQuote + ",") > 0) {
-          evalObjectElements.push(objStr.substring(1, objStr.indexOf(firstQuote + ",")));
+          evalArrayElements.push(objStr.substring(1, objStr.indexOf(firstQuote + ",")));
           objStr = objStr.slice(objStr.indexOf(firstQuote + ",") + 2);
           objStr = objStr.trim()
         }
         else if (objStr.lastIndexOf(firstQuote) === objStr.length-1) {
-            evalObjectElements.push(objStr.substring(1, objStr.length-1));
+            evalArrayElements.push(objStr.substring(1, objStr.length-1));
             objStr = "";
         }
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //function es5 es6
@@ -114,7 +114,7 @@ function evalObject(objStr, evalObjectElements) {
         objStr.search(RegExp(es6FunctionRegEx + ",")) !== -1){
           console.log("In if for functions");
           params.push(objStr.slice(objStr.indexOf("{") + 1, objStr.indexOf("}, ")));
-          evalObjectElements.push(new Function(...params));
+          evalArrayElements.push(new Function(...params));
           objStr = objStr.slice(objStr.indexOf("}, ") + 3).trim();
           console.log("objStr is", objStr);
         }
@@ -123,11 +123,11 @@ function evalObject(objStr, evalObjectElements) {
         RegExp(es6FunctionRegEx).test(objStr) && objStr.match(RegExp(es6FunctionRegEx))[0].length === objStr.length) {
           console.log("in else if for functions");
           params.push(objStr.slice(objStr.indexOf("{") + 1, objStr.lastIndexOf("}")));
-          evalObjectElements.push(new Function(...params));
+          evalArrayElements.push(new Function(...params));
           objStr = "";
           console.log(objStr);
         }
-        return evalObject("{" + objStr + "}", evalObjectElements);
+        return evalObject("{" + objStr + "}", evalArrayElements);
       }
 
       //array
@@ -137,13 +137,13 @@ function evalObject(objStr, evalObjectElements) {
         if (sliceArrObj("{", "}", objStr)) {
           let objStrSubstring = objStr.substring(0, this.sliceArrObj("{", "}", objStr));
           console.log("objStrSubstring is", objStrSubstring);
-          //Push in evalObjectElements array the function for evaluating arrays or objects (recursively), with the related substring
-          evalObjectElements.push(evalObject(objStrSubstring, []));
+          //Push in evalArrayElements array the function for evaluating arrays or objects (recursively), with the related substring
+          evalArrayElements.push(evalObject(objStrSubstring, []));
 
           //slice the arrayString at the level of the substring
           objStr = objStr.slice(sliceArrObj("{", "}", objStr) + 1);
           objStr = objStr.trim();
-          return evalObject("{" + objStr + "}", evalObjectElements);
+          return evalObject("{" + objStr + "}", evalArrayElements);
         }
       }
 
@@ -154,8 +154,8 @@ function evalObject(objStr, evalObjectElements) {
         if (sliceArrObj("{", "}", objStr)) {
           let objStrSubstring = objStr.substring(0, this.sliceArrObj("{", "}", objStr));
           console.log("objStrSubstring is", objStrSubstring);
-          //Push in evalObjectElements array the function for evaluating arrays or objects (recursively), with the related substring
-          evalObjectElements.push(evalObject(objStrSubstring, []));
+          //Push in evalArrayElements array the function for evaluating arrays or objects (recursively), with the related substring
+          evalArrayElements.push(evalObject(objStrSubstring, []));
 
           //slice the arrayString at the level of the substring
           objStr = objStr.slice(sliceArrObj("{", "}", objStr) + 1);
--- genericRuleFunction.js
+++ genericRuleFunction.js
@@ -1,80 +1,3 @@
-var str1 = `1 $100 Mail-In Rebate includes Grabber™ X3, Grabber™ A/TX, Grabber™ APT, Grabber™ AT2.
-
-2 $70 Mail-In Rebate includes Grabber™, Grabber™ HTS60, Grabber™ HD, G-MAX™ RS, G-MAX™ AS-05, G-MAX™ AS-03, Grabber™ UHP.
-
-3 $50 Mail-In Rebate includes AltiMAX™ Arctic, AltiMAX™ Arctic12, AltiMAX™ HP, AltiMAX™ RT, AltiMAX™ RT43, Ameritrac™, Ameritrac™ TR, Grabber™ AW, Grabber™ HP, Grabber™ HTS, Grabber™ TR, GRABBER™ Arctic LT, GRABBER™ Arctic.
-
-*See associate for complete offer details. Mail-in rebates are in the form of a Tire Pros Visa Prepaid Card. Offer valid only with qualified purchases made at participating Tire Pros locations in the forty-eight (48) contiguous continental U.S. and D.C. between November 1 and November 30, 2018.  Void where prohibited by law. No rain checks. Consumer receives up to $100 Prepaid Card for the purchase of four (4) new qualifying General Tire passenger or light truck tires. Consumer receives an additional $50 rebate on the Visa Prepaid Card when the qualifying General Tire purchase is made using a Tire Pros Credit Card (subject to credit approval). Valid with the original, dated, paid receipt from a participating Tire Pros retailer and a completed redemption form. Offer may not be combined with any other offer, discount, rebate, or promotion. Tires must be purchased in a single transaction. Offer not valid on purchases by wholesalers, dealers, fleets, OEM customers, or other commercial entities. Rebate form must be submitted online or postmarked by December 30, 2018. Please allow 6-8 weeks for delivery of your Visa Prepaid Card. If Visa Prepaid Card is not received, you must inquire no later than February 22, 2019 or all rights therein will expire. Requests from those with an invalid or undeliverable mailing address will be denied. Limit two (2) per household. Fraudulent submission and/or multiple requests could result in Federal prosecution under the U.S. Mail Fraud Statutes (18 USC. Section 1341 and 1342). The Visa Prepaid Cards are issued by MetaBank®, Member FDIC, pursuant to a license from Visa U.S.A. Inc. No cash access or recurring payments. Can be used everywhere Visa debit cards are accepted. Card valid for up to 6 months; unused funds will be forfeited at midnight EST the last day of the month of the valid thru date. Card terms and conditions apply, see MyPrepaidCenter.com/site/visa-promo.  MetaBank does not sponsor or endorse the optional Tire Credit Card.`;
-
-var str2 = "Buy 3 t-shirts, get an extra one free";
-
-var str3 = "Save 10% on sweaters. Valid from 10/22/2018 on 12:01am E.T. through 10/25/2018 on 11:59pm E.T.m May not be combined with other promotions.";
-
-var str4 = "You will fall in love with these new offers | Get an extra $15 off on us on all sale items while you use your Fabulous card | Shop now!";
-
-var str5 = "Dear Ethan, we missed you. As a welcome back gift, get a $5 coupon to use on any regular priced item. Offer expires in two days.";
-
-var str6 = "• $1 = 15 points* • 2500 points = $10 reward • Buy 5 jeans, get one free**";
-
-var str7 = `*Purchases subject to credit approval. Must use your AEO Connected, AEO or Aerie Visa Credit Card to earn points anywhere Visa credit cards are accepted. Earn 15 points per $1 spent at AEO® and Aerie® stores and ae.com and aerie.com after discounts, taxes and fees or earn 5 points per $1 spent outside AEO® and Aerie® stores and ae.com and aerie.com. Excludes gift cards. See AEO Connected Rewards Program Terms and Conditions for details at ae.com/aeoconnected/terms.
-
-**Clearance jeans and bras are excluded from the five needed to earn this benefit.
-
-***Free standard shipping to U.S., Puerto Rico and Canada.
-
-Head off identity theft before it happens. For more information about identity theft, please visit the Federal Trade Commission's (FTC) consumer website.
-
-This is an unmonitored email box. Please do not reply to this email.
-
-NOTE: This email was sent by Synchrony Bank's AEO® Credit Services Division, the issuer of your AEO Connected Visa Credit Card account. Synchrony Bank is located at 170 Election Road, Suite 125, Draper, UT 84020-6425, USA. www.synchronyfinancial.com.
-
-Unsubscribe: If you no longer wish to receive email marketing communications from Synchrony Bank's AEO® Credit Services Division, please click here to unsubscribe.
-
-The AEO Connected, AEO and Aerie Visa Credit Cards are issued by Synchrony Bank pursuant to a license from Visa USA Inc.
-
-© 2018 AEO Management Co. All rights reserved.`;
-
-var str8 = "There is no place like home! Grab an opportunity to save. All towels and rugs start at $10.99. Hurry! Offer expires in two days.";
-
-var str9 = "Save big on pillows. Was $29.99, now $15.99. It feels good to save!";
-
-var str9a = "Save big on pillows. Was $29.99, now $15.99. It feels good to save! Plus, a special gift from us. While supplies last";
-
-var str10 = "Big sale is on! Save 20% on everything! Plus, free shipping on all purchases that are $50 or more";
-
-var str11 = "Earn 15 points per $1 spent at AEO® and Aerie® stores and ae.com and aerie.com after discounts, taxes and fees or earn 5 points per $1 spent outside AEO® and Aerie® stores and ae.com and aerie.com. Excludes gift cards. See AEO Connected Rewards Program Terms and Conditions for details at ae.com/aeoconnected/terms.";
-
-var str12 = "30% off everything online or in stores. Use Code LANTERN. This special, limited purchase offer starts 8/24/2017 and expires 8/29/2017. Offers exclude custom-ordered products, Evertherm products, all sunglasses, tents, sleeping bags, First Ascent Packs and non-Eddie Bauer brand products. Offer at Eddie Bauer Outlet and Retail stores may vary, see store for details. Offer may not be valid in stores outside the United States. Offer does not include and cannot be applied to previous, pending or post-dated purchases, credit card balances, taxes, monogramming charges, gift cards, or gift boxes. Unless explicitly stated above, offers cannot be combined with any other offer. Offer details, including end date, are subject to change or cancellation without notice.";
-
-
-
-
-//NEGATIVES - SHOULD RETURN NULL when the variable negRegexArr is populated with the related keywords.
-var str13 = "Valid one time only on your purchase at U.S. And Canada Eddie Bauer stores, eddiebauer.com (if shipped to a U.S. Address), and through our catalogs. May not be exchanged for cash, used for credit card payments, or replaced if lost or stolen, except as required by law. May not be sold, auctioned, transferred, or combined with any other offer. VOID if altered or reproduced. Not valid on gift certificates/cards, or on prior or pending purchases. We reserve the right to cancel an order for unauthorized use. We may modify or cancel this promotion due to an error or unforeseen problems. Unused certificate amounts not payable in cash or subsequently redeemable. Cash value: 1/100 of a cent.";
-
-var str14 = "(if shipped to a U.S. Address), and through our catalogs. May not be exchanged for cash, used for credit card payments, or replaced if lost or stolen, except as required by law. May not be sold, auctioned, transferred, or combined with any other offer. VOID if altered or reproduced. Not valid on gift certificates/cards, or on prior or pending purchases. We reserve the right to cancel an order for unauthorized use. We may modify or cancel this promotion due to an error or unforeseen problems. Unused certificate amounts not payable in cash or subsequently redeemable. Cash value: 1/100 of a cent.";
-
-var str15 = "NO PURCHASE NECESSARY. Legal residents of the 50 United States (D.C.) & CA (excluding QC), 18 years or older. Enter Contest by 9/30/18. For Official Rules, including prize descriptions, visit www.eddiebauer.com/WhyIHike. 25 winners each month (prize valued at $100 USD/CAD) and 1 Grand Prize (valued at approximately $16,000 USD). Void where prohibited.";
-
-var str16 = "* 30% offer valid at eddiebauer.com, at 800.426.8020, and Eddie Bauer stores, through 02/26/2012. Offer not valid on non-Eddie Bauer branded watches, sunglasses, gear, gadgets, tools, shoes, and accessories. Does not include and cannot be applied to previous or pending purchases, credit card balance, taxes, shipping or monogramming charges, gift cards or gift boxes. Offer cannot be combined with any other offer.";
-
-var str17 = "*Eddie Bauer Credit Cards are issued by Comenity Bank. Subject to credit approval. Must open and use an Eddie Bauer Credit Card by 1/29 to receive $20 Reward Certificate.This reward is issued for promotional purposes. Certificate cannot be applied to prior purchases. Certificate cannot be used as payment on Eddie Bauer credit card accounts.";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function transform(data){
   var negRegexArr = [
 
@@ -113,7 +36,7 @@ function transform(data){
     // (double | triple | NN times the) (points)
     /(double|triple|\d\s*times\s*the|\dx(\s*the)?)\s*(points)/i,
     //promo(tion) code
-    /promo(?:tion)\s*code\s*/i,
+    /promo(?:tion)\s*code/i,
   ];
 
   for (var j = 0; j < posRegexArr.length; j++) {
@@ -173,15 +96,25 @@ function minimizeMe(str, reg){
 function cleanMe(string) {
 
   var replaceStrArr = [
-    //{oldStr: //, newStr: "$1 $2"},
+    //FULL TEXT REMOVAL
+    // {oldStr: /.*().*/i, newStr: ""}, // .*Find text in the middle of string.*
+    // {oldStr: /^().*/i, newStr: ""}, // ^Find text in the beginning of the string.*
+    // {oldStr: /.*()$/i, newStr: ""}, // .*Find text in the end of the string$
+    // {oldStr: /^()$/i, newStr: ""}, // ^Find text at the beginning and the end of the string$
+    //
+    // PARTIAL TEXT REMOVAL
+    // {oldStr: /^()/i, newStr: ""}, // ^Find text at the beginning of the string
+    // {oldStr: /()$/i, newStr: ""}, // Find text at the end of the string$
+    // {oldStr: /.*()/i, newStr: ""}, // .*Find text in the middle, remove everything before that
+    // {oldStr: /().*/i, newStr: ""}, // Find text in the middle, remove everything after that.*
+    // {oldStr: /()/i, newStr: ""}, // Find text in the middle of the string
   ];
 
   for (var i = 0; i < replaceStrArr.length; i++) {
     string = string.replace(replaceStrArr[i].oldStr, replaceStrArr[i].newStr).trim();
   }
 
-  while("*©®ǂ‡†±+→§™¹›∞•◊ΔÐð_|^".indexOf(string[string.length-1]) !== -1) string = string.slice(0, string.length-1);
-  while("*©®ǂ‡†±+→§™¹›∞•◊ΔÐð_^".indexOf(string[0]) !== -1) string = string.slice(1);
+  string = string.replace(/[¶\*©®ǂ‡^†±→§™¹›]/g, "");
 
 
   return string;
